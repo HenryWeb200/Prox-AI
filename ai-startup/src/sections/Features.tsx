aadf48e4c495fb478bed9d700df823aade4c0182
HenryDevOps <ikechukwuinyamah@gmail.com>
2025-06-29 22:26:06 +0100

    First initial commit

---
+++ ai-startup/src/sections/Features.tsx
@@ -0,0 +1,34 @@
+const tabs = [
+  {
+    icon: "/assets/lottie/vroom.lottie",
+    title: "User-friendly dashboard",
+    isNew: false,
+    backgroundPositionX: 0,
+    backgroundPositionY: 0,
+    backgroundSizeX: 150,
+  },
+  {
+    icon: "/assets/lottie/click.lottie",
+    title: "One-click optimization",
+    isNew: false,
+    backgroundPositionX: 98,
+    backgroundPositionY: 100,
+    backgroundSizeX: 135,
+  },
+  {
+    icon: "/assets/lottie/stars.lottie",
+    title: "Smart keyword generator",
+    isNew: true,
+    backgroundPositionX: 100,
+    backgroundPositionY: 27,
+    backgroundSizeX: 177,
+  },
+];
+
+import React from "react";
+
+const Features = () => {
+  return <div>Features</div>;
+};
+
+export default Features;
